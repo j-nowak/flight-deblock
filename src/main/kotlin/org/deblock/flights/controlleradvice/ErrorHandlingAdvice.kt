abd4779c2cd3243247ccc94843a6ed66050302c1
j-nowak <pl.jakub.nowak@gmail.com>
2024-01-28 10:36:56 +0100

    Refactor ErrorHandlingAdvice

--- src/main/kotlin/org/deblock/flights/controlleradvice/ErrorHandlingAdvice.kt
+++ src/main/kotlin/org/deblock/flights/controlleradvice/ErrorHandlingAdvice.kt
@@ -1,5 +1,4 @@
 package org.deblock.flights.controlleradvice
-
 import com.fasterxml.jackson.databind.exc.InvalidFormatException
 import com.fasterxml.jackson.databind.exc.ValueInstantiationException
 import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
@@ -11,77 +10,79 @@ import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.lang.IllegalArgumentException
 
 @RestControllerAdvice
 class ErrorHandlingAdvice {
 
-    @ExceptionHandler(ConstraintViolationException::class)
+    @ExceptionHandler(ConstraintViolationException::class, MethodArgumentNotValidException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleConstraintViolationException(ex: ConstraintViolationException): ResponseEntity<ErrorResponse> {
-        val details = ex.constraintViolations.map { it.message }
-        val errorResponse = ErrorResponse("Invalid request body", details)
-        return ResponseEntity.badRequest().body(errorResponse)
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleConstraintViolationException(ex: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
-        val details = ex.bindingResult.fieldErrors.map { it.defaultMessage ?: "Validation failed" }
-        val errorResponse = ErrorResponse("Invalid request body", details)
-        return ResponseEntity.badRequest().body(errorResponse)
+    fun handleValidationException(ex: Exception): ResponseEntity<ErrorResponse> {
+        val details = when (ex) {
+            is ConstraintViolationException -> ex.constraintViolations.map { it.message }
+            is MethodArgumentNotValidException -> ex.bindingResult.fieldErrors.map { it.defaultMessage }
+            else -> emptyList()
+        }
+        return ResponseEntity.badRequest().body(ErrorResponse.invalidRequestBody(details))
     }
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleConstraintViolationException(ex: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
-        if (ex.cause is ValueInstantiationException) {
-            return handleValueInstantiationException(ex.cause as ValueInstantiationException)
-        } else if (ex.cause is MissingKotlinParameterException) {
-            return handleMissingParameter(ex.cause as MissingKotlinParameterException)
-        } else if (ex.cause is InvalidFormatException) {
-            return handleInvalidFormatException(ex.cause as InvalidFormatException)
+    fun handleHttpMessageNotReadableException(ex: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
+        return when (val cause = ex.cause) {
+            is ValueInstantiationException -> handleValueInstantiationException(cause)
+            is MissingKotlinParameterException -> handleMissingParameter(cause)
+            is InvalidFormatException -> handleInvalidFormatException(cause)
+            else -> ResponseEntity.badRequest().body(ErrorResponse.invalidRequestBody())
         }
-//        val details = ex
-        val errorResponse = ErrorResponse("Invalid request body")
-        return ResponseEntity.badRequest().body(errorResponse)
     }
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleException(ex: Exception): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse("Internal server error")
-        return ResponseEntity.internalServerError().body(errorResponse)
+        return ResponseEntity.internalServerError().body(ErrorResponse.internalServerError())
     }
 
     private fun handleValueInstantiationException(ex: ValueInstantiationException): ResponseEntity<ErrorResponse> {
-        val errorResponse = if (ex.cause is IllegalArgumentException && ex.cause?.message != null) {
-            val details = (ex.cause as IllegalArgumentException).message!!
-            ErrorResponse("Invalid request body", listOf(details))
+        val details = if (ex.cause is IllegalArgumentException && ex.cause?.message != null) {
+            listOf((ex.cause as IllegalArgumentException).message!!)
         } else {
-            ErrorResponse("Invalid request body")
+            emptyList()
         }
-        return ResponseEntity.badRequest().body(errorResponse)
+        return ResponseEntity.badRequest().body(
+            ErrorResponse.invalidRequestBody(details)
+        )
     }
 
     private fun handleMissingParameter(ex: MissingKotlinParameterException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(
-            "Invalid request body",
-            listOf("Missing request body parameter: ${ex.path.first().fieldName}")
+        return ResponseEntity.badRequest().body(
+            ErrorResponse.invalidRequestBody(
+                "Missing request body parameter: ${ex.path.first().fieldName}"
+            )
         )
-        return ResponseEntity.badRequest().body(errorResponse)
     }
 
     private fun handleInvalidFormatException(ex: InvalidFormatException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(
-            "Invalid request body",
-            listOf("Invalid format for parameter: ${ex.path.first().fieldName}")
+        return ResponseEntity.badRequest().body(
+            ErrorResponse.invalidRequestBody("Invalid format for parameter: ${ex.path.first().fieldName}")
         )
-        return ResponseEntity.badRequest().body(errorResponse)
     }
 }
 
 data class ErrorResponse(
     val error: String,
     val details: List<String> = emptyList()
-)
+) {
+    companion object {
+        fun invalidRequestBody(errorDetails: List<String> = emptyList()): ErrorResponse {
+            return ErrorResponse("Invalid request body", errorDetails)
+        }
+
+        fun invalidRequestBody(errorDetails: String): ErrorResponse {
+            return invalidRequestBody(listOf(errorDetails))
+        }
+
+        fun internalServerError(): ErrorResponse {
+            return ErrorResponse("Internal server error")
+        }
+    }
+}
